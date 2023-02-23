8963e03bbba2288029a99cb6f2ff8ba803a70fe0
Clement <clement.pene@sii.fr>
2023-02-23 16:20:38 +0100

    TEST SERVO : running ?

--- examples/two_liasons_ra.rs
+++ examples/two_liasons_ra.rs
@@ -7,7 +7,8 @@ fn main() {
 
     let i2c = I2c::new().expect("1");
     let mut ms = MotorShield::new(i2c).expect("I2C not detected.");
-
-    ms.set_servo(ServoNumber::S1, 33.3).unwrap();
-
+    loop {
+        ms.set_servo(ServoNumber::S1, 33.3).unwrap();
+    }
+    
 }
