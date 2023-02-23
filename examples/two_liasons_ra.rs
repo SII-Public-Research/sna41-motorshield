be04f8c484387fdb3552203278a45a0aabecbadf
Clement <clement.pene@sii.fr>
2023-02-23 16:56:23 +0100

    TEST SERVO : multiple positions

--- examples/two_liasons_ra.rs
+++ examples/two_liasons_ra.rs
@@ -1,14 +1,21 @@
+use embedded_hal::prelude::_embedded_hal_blocking_delay_DelayMs;
 // test connexion
 use rppal::i2c::I2c;
+use rppal::hal::Delay;
 use sna41_motorshield::{servo::ServoNumber, MotorShield};
 
 
 fn main() {
 
+    let mut delay = Delay::new();
+
     let i2c = I2c::new().expect("1");
     let mut ms = MotorShield::new(i2c).expect("I2C not detected.");
     loop {
-        ms.set_servo(ServoNumber::S2, 33.3).unwrap();
+        ms.set_servo(ServoNumber::S2, 30.0).unwrap();
+        delay.delay_ms(1000_u64);
+        ms.set_servo(ServoNumber::S2, 40.0).unwrap();
+        delay.delay_ms(1000_u64);
     }
     
 }
