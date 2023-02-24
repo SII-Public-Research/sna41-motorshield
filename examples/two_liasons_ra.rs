61923d3b777caa86b47dd73251c5b555c05587cd
Clement <clement.pene@sii.fr>
2023-02-24 15:14:35 +0100

    TEST SERVO : use pca crate directly on example

--- examples/two_liasons_ra.rs
+++ examples/two_liasons_ra.rs
@@ -1,8 +1,12 @@
+use std::ops::Add;
+
 use embedded_hal::prelude::_embedded_hal_blocking_delay_DelayMs;
 // test connexion
 use rppal::i2c::I2c;
 use rppal::hal::Delay;
 use sna41_motorshield::{servo::ServoNumber, MotorShield};
+
+use pwm_pca9685::{Address, Channel, Pca9685};
 
 
 fn main() {
@@ -10,12 +14,25 @@ fn main() {
     let mut delay = Delay::new();
 
     let i2c = I2c::new().expect("1");
-    let mut ms = MotorShield::new(i2c).expect("I2C not detected.");
+    // let mut ms = MotorShield::new(i2c).expect("I2C not detected.");
+    let address = Address::default();
+    let mut pwm = Pca9685::new(i2c, address).unwrap();
+
+    pwm.set_prescale(100).unwrap();
+
+    pwm.enable().unwrap();
+
+    pwm.set_channel_on(Channel::C0, 0).unwrap();
+
+    pwm.set_channel_off(Channel::C0, 2047).unwrap();
+
     loop {
-        ms.set_servo(ServoNumber::S2, 30.0).unwrap();
-        delay.delay_ms(1000_u64);
-        ms.set_servo(ServoNumber::S2, 40.0).unwrap();
-        delay.delay_ms(1000_u64);
+        // ms.set_servo(ServoNumber::S2, 30.0).unwrap();
+        // delay.delay_ms(1000_u64);
+        // ms.set_servo(ServoNumber::S2, 40.0).unwrap();
+        // delay.delay_ms(1000_u64);
+
+
     }
     
 }
