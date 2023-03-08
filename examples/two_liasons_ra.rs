4b86677c4097affbf4a1a71c3c1ac2839be87252
Clement <clement.pene@sii.fr>
2023-03-08 09:32:28 +0100

    Test servo

--- examples/two_liasons_ra.rs
+++ examples/two_liasons_ra.rs
@@ -29,9 +29,14 @@ fn main() {
         pwm.set_channel_on_off(Channel::C0, 0, 105).unwrap();
         delay.delay_ms(2000_u64);
 
-        pwm.set_channel_on_off(Channel::C1, 0, 655).unwrap();
+        pwm.set_channel_on_off(Channel::C1, 0, 400).unwrap();
         delay.delay_ms(2000_u64);
-        pwm.set_channel_on_off(Channel::C1, 0, 105).unwrap();
+        pwm.set_channel_on_off(Channel::C1, 0, 255).unwrap();
+        delay.delay_ms(2000_u64);
+
+        pwm.set_channel_on_off(Channel::C2, 0, 400).unwrap();
+        delay.delay_ms(2000_u64);
+        pwm.set_channel_on_off(Channel::C2, 0, 255).unwrap();
         delay.delay_ms(2000_u64);
     }
     
