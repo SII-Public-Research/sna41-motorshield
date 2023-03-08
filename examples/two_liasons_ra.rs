be9e00e1587f9a9eaaaeb9f0bf3033158f3beb5f
Clement <clement.pene@sii.fr>
2023-03-08 12:30:00 +0100

    SERVO : added function to control the servo with angle value.

--- examples/two_liasons_ra.rs
+++ examples/two_liasons_ra.rs
@@ -33,6 +33,8 @@ fn main() {
         // delay.delay_ms(2000_u64);
         // pwm.set_channel_on_off(Channel::C2, 0, 105).unwrap();
         // delay.delay_ms(2000_u64);
+        ms.set_servo_angle(ServoNumber::S0, 0.0).unwrap();
+        ms.set_servo_angle(ServoNumber::S1, 0.0).unwrap();
 
         ms.set_servo_angle(ServoNumber::S2, 0.0).unwrap();
         delay.delay_ms(2000_u64);
